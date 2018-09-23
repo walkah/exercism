0a632f27dd1308ca6770c4740019d9276baaa631
James Walker <walkah@walkah.net>
2018-09-23 17:26:38 -0500

    ruby - acronym
    * split -> scan much cleaner!

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,5 @@
 class Acronym
   def self.abbreviate(long_name)
-    abbrev = ""
-    long_name.split(/[\W-]+/).each do |word|
-      abbrev += word[0]
-    end
-    abbrev.upcase
+    long_name.scan(/\b\w/).join.upcase
   end
 end
