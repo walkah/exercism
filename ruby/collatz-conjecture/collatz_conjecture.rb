0e1e6a0d9cf49185a064ea85c4a54ea1da156d90
James Walker <walkah@walkah.net>
2018-10-22 11:49:29 -0400

    ruby - collatz-conjecture

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,17 +1,11 @@
+# CollatzConjecture class
 class CollatzConjecture
-  def self.steps(n, count = 0)
-    if n < 1
-      raise ArgumentError
-    elsif n == 1
-      count
-    else
-      count += 1
-      if n % 2 == 0
-        number = n / 2
-      else
-        number = (3 * n) + 1
-      end
-      steps(number, count)
-    end
+  def self.steps(num, count = 0)
+    raise ArgumentError if num < 1
+    return count if num == 1
+
+    count += 1
+    number = num.even? ? num / 2 : (3 * num) + 1
+    steps(number, count)
   end
 end
