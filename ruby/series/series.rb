1eb8eb0e30df28e5a2e149881799260274c00892
James Walker <walkah@walkah.net>
2018-10-22 11:23:17 -0400

    ruby - series
    rubocop cleanup

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,12 +1,11 @@
+# Series class
 class Series
   def initialize(digits)
     @digits = digits
   end
 
   def slices(slice_length)
-    if slice_length > @digits.length
-      raise ArgumentError
-    end
+    raise ArgumentError if slice_length > @digits.length
 
     slices = []
     0.upto(@digits.length - slice_length) do |i|
