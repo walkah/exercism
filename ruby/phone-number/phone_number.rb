fdb0f51e586fb07ebc564bd85c691e3e6e365095
James Walker <walkah@walkah.net>
2018-10-22 11:44:36 -0400

    ruby - phone-number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,19 +1,9 @@
+# PhoneNumber class
 class PhoneNumber
   def self.clean(number)
-    number.gsub!(/[^0-9]/, '')
+    number = number.gsub(/\D/, '').sub(/^[01]/, '')
+    return nil if number.length != 10 || %w[0 1].include?(number[3])
 
-    if number.length < 10 or number.length > 11
-      nil
-    elsif number.length == 11
-      if number[0] == "1"
-        number.slice(1, number.length)
-      else
-        nil
-      end
-    elsif ["0", "1"].include?(number[0]) or ["0", "1"].include?(number[3])
-      nil
-    else
-      number
-    end
+    number
   end
 end
